82fb460da8456477e6d933f8a13ad5846bdd7eb6
Elena <elenafayethomas@gmail.com>
2022-11-14 15:35:25 +0000

    feat(page): skills page draft completed

--- components/skills.js
+++ components/skills.js
@@ -1,8 +1,25 @@
-import Image from "next/image"
-// static content imports
-import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
+import {
+    IoLogoPython,
+    IoLogoJavascript
+} from "react-icons/io"
+import {
+    SiAmazonaws,
+    SiConfluence,
+    SiDocker,
+    SiElastic,
+    SiGit,
+    SiGo,
+    SiIbm,
+    SiJira,
+    SiMarkdown,
+    SiNewrelic,
+    SiOpsgenie,
+    SiTerraform
+} from "react-icons/si"
+import {
+    VscTerminalBash
+} from "react-icons/vsc"
+
 
 export default function skills() {
     return (
@@ -10,43 +27,172 @@ export default function skills() {
             <div>
                 <h3 className="text-3xl py-1">Skills I offer</h3>
                 <p className="text-md py-2 leading-8 text-gray-800 dark:text-slate-200">
-                <em>Scripting, AWS and random facts of the day </em>😊
+                <div className="text-center">
+                    <em>Scripting, DevSecOps know-how and random facts of the day </em>😊
+                </div>
                 </p>
             </div>
             <div className="lg:flex gap-10">
-                <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image alt="design" className="" src={design}></Image>
-                <h3 className="text-lg font-medium pt-8 pb-2">Tools</h3>
-                <p>
-                    Tools go here
-                </p>
-                {/* TODO: insert urls for tools/languages etc. */}
-                <h4 className="py-4 text-teal-600 dark:text-teal-200">Tools I use</h4>
-                <p className="text-gray-800 dark:text-white py-1">AWS</p>
-                <p className="text-gray-800 dark:text-white py-1">Git</p>
-                <p className="text-gray-800 dark:text-white py-1">Atlassian Suite</p>
+                <div className="justify-center text-center shadow-lg p-10 rounded-xl my-10">
+                    <h3 className="text-lg font-semibold pt-8 pb-2">Tools</h3>
+                    {/* TODO: insert urls for tools/languages etc. */}
+                    <div className="grid grid-cols-1 sm:max-lg:grid-cols-3">
+                        <div>
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Containerisation</h4>
+                            <div className="inline-flex">
+                                <p>Docker &nbsp;</p>
+                                <div className="text-blue-600 dark:text-blue-300 pt-1.5">
+                                    <SiDocker></SiDocker>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="inline-flex">
+                                <p>Elastic Container Service &nbsp;</p>
+                                <div className="text-orange-700 dark:text-orange-200 pt-1.5">
+                                    <SiElastic></SiElastic>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="lg:pt-5">
+                            <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Infrastructure as Code</h4>
+                            <div className="inline-flex">
+                                <p>Terraform &nbsp;</p>
+                                <div className="text-purple-700 dark:text-purple-300 pt-1.5">
+                                    <SiTerraform></SiTerraform>
+                                </div>
+                            </div>
+                            <br></br>
+                        </div>
+                            <div className="inline-flex">
+                                <p>AWS CDK &nbsp;</p>
+                                <div className="text-orange-400 dark:text-orange-200 pt-1.5">
+                                    <SiAmazonaws></SiAmazonaws>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Monitoring & Observability</h4>
+                            <div className="inline-flex">
+                                <p>New Relic &nbsp;</p>
+                                <div className="text-teal-600 dark:text-teal-200 pt-1.5">
+                                    <SiNewrelic></SiNewrelic>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image alt="code"  src={code}></Image>
-                <h3 className="text-lg font-medium pt-8 pb-2">Languages</h3>
-                <p>
-                    Languages go here
-                </p>
-                <h4 className="py-4 text-teal-600 dark:text-teal-200">Languages that I use</h4>
-                <p className="text-gray-800 dark:text-white py-1">Python</p>
-                <p className="text-gray-800 dark:text-white py-1">Terraform</p>
-                <p className="text-gray-800 dark:text-white py-1">Bash</p>
+                    <h3 className="text-lg font-semibold pt-8 pb-2">Languages</h3>
+                    {/* TODO: insert urls for tools/languages etc. */}
+                    <div className="grid grid-cols-1 sm:max-lg:grid-cols-2">
+                        <div>
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Scripting</h4>
+                            <div className="inline-flex">
+                                <p>Python &nbsp;</p>
+                                <div className="text-blue-700 dark:text-blue-200 pt-1.5">
+                                    <IoLogoPython></IoLogoPython>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="inline-flex">
+                                <p>JavaScript &nbsp;</p>
+                                <div className="text-yellow-500 dark:text-yellow-200 pt-1.5">
+                                    <IoLogoJavascript></IoLogoJavascript>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="inline-flex">
+                                <p>GNU Bash &nbsp;</p>
+                                <div className="text-red-600 dark:text-red-300 pt-1.5">
+                                    <VscTerminalBash></VscTerminalBash>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Compiled</h4>
+                            <div className="inline-flex">
+                                <p>Golang &nbsp;</p>
+                                <div className="text-blue-400 dark:text-blue-200 pt-1.5">
+                                    <SiGo></SiGo>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="justify-center text-center shadow-lg p-10 rounded-xl my-10">
+                    <h3 className="text-lg font-semibold pt-8 pb-2">Versioning & Documentation</h3>
+                    {/* TODO: insert urls for tools/languages etc. */}
+                    <div className="grid grid-cols-1 sm:max-lg:grid-cols-3">
+                        <div>
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Version Control</h4>
+                            <div className="inline-flex">
+                                <p>Git &nbsp;</p>
+                                <div className="text-orange-600 dark:text-orange-300 pt-1.5">
+                                    <SiGit></SiGit>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="inline-flex">
+                                <p>IBM RTC &nbsp;</p>
+                                <div className="text-cyan-700 dark:text-cyan-200 pt-1.5">
+                                    <SiIbm></SiIbm>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="lg:pt-5">
+                            <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Documentation</h4>
+                            <div className="inline-flex">
+                                <p>Confluence &nbsp;</p>
+                                <div className="text-blue-600 dark:text-blue-300 pt-1.5">
+                                    <SiConfluence></SiConfluence>
+                                </div>
+                            </div>
+                            <br></br>
+                        </div>
+                            <div className="inline-flex">
+                                <p>Docs as code &nbsp;</p>
+                                <div className="text-black dark:text-white pt-1.5">
+                                    <SiMarkdown></SiMarkdown>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Work Management</h4>
+                            <div className="inline-flex">
+                                <p>Jira &nbsp;</p>
+                                <div className="text-blue-600 dark:text-blue-200 pt-1.5">
+                                    <SiJira></SiJira>
+                                </div>
+                            </div>
+                            <br></br>
+                            <div className="inline-flex">
+                                <p>OpsGenie &nbsp;</p>
+                                <div className="text-teal-600 dark:text-teal-200 pt-1.5">
+                                    <SiOpsgenie></SiOpsgenie>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="text-center shadow-lg p-10 rounded-xl my-10">
-                <Image alt="consulting"  src={consulting}></Image>
-                <h3 className="text-lg font-medium pt-8 pb-2">Qualifications</h3>
-                <p>
-                    Qualification go here
-                </p>
-                <h4 className="py-4 text-teal-600 dark:text-teal-200">Tools I use</h4>
-                <p className="text-gray-800 dark:text-white py-1">Qualification one</p>
-                <p className="text-gray-800 dark:text-white py-1">Qualification two</p>
-                <p className="text-gray-800 dark:text-white py-1">Qualification three</p>
+                    <h3 className="text-lg font-semibold pt-8 pb-2">Qualifications</h3>
+                    {/* TODO: insert urls for tools/languages etc. */}
+                    <div className="grid grid-cols-1 sm:max-lg:grid-cols-3">
+                        <div>
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Cloud</h4>
+                            <p>::qualification::</p>
+                        </div>
+                        <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Security</h4>
+                            <p>::qualification::</p>
+                        </div>
+                        <div className="lg:pt-5">
+                            <h4 className="font-semibold text-teal-600 dark:text-teal-200">Monitoring & Observability</h4>
+                            <p>::qualification::</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
