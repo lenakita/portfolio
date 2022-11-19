b3271ee8274143f4a292c811022361a0e668e365
Elena <elenafayethomas@gmail.com>
2022-11-19 13:16:00 +0000

    fix(html): updated tags to className from class

--- pages/footer.js
+++ pages/footer.js
@@ -11,9 +11,9 @@ import {
 export default function footer() {
     return(
         <div className="pb-5">
-            <footer class="p-4 md:p-6 bg-slate-100 rounded-lg dark:bg-gray-800">
+            <footer className="p-4 md:p-6 bg-slate-100 rounded-lg dark:bg-gray-800">
                 <p className="pl-2 md:pl-16 text-md md:text-lg text-slate-800 dark:text-slate-200">Resources used</p>
-                <ul class="pl-2 md:pl-20 flex flex-col space-y-1 items-left mt-3 text-sm md:text-md text-gray-500 dark:text-gray-400 sm:mt-0">
+                <ul className="pl-2 md:pl-20 flex flex-col space-y-1 items-left mt-3 text-sm md:text-md text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li className="inline-flex">
                         <div className="pt-1 text-yellow-500 dark:text-yellow-200">
                             <TbBrandNextjs/>
